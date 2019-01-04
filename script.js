53682168e93523372f3b97047abeeda127074445
anita-hu <anitahu113@gmail.com>
2019-01-03 21:51:45 -0500

    added animations

---
+++ script.js
@@ -0,0 +1,8 @@
+// smooth nav scrolling
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 50
+    }, 500);
+});
