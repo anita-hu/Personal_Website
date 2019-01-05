4b7ba27f423dfacdbe9ac152738c775abd30e167
anita-hu <anitahu113@gmail.com>
2019-01-05 15:11:30 -0500

    style update

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top - 50
     }, 500);
 });
+
+$(document).ready(function(){
+  $("#toggle").click(function(){
+    $("#more").slideToggle();
+    $(this).toggleClass("expanded");
+
+    if ($(this).hasClass("expanded")) {
+        $(this).html("Show Less");
+    } else {
+        $(this).html("Show More");
+    }
+  });
+});
